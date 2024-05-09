f2a7b14f028c853819b0ef05eb3ed103eccac019
Timingcode <96377154+Timingcode@users.noreply.github.com>
2024-05-09 18:47:24 +0000

    komentar

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     //kontrola ci uz bol potvrdeny formular a ci boli vyplnene obidva udaje aj username aj password
    if (isset($_POST['login']) && !empty($_POST['username']) 
       && !empty($_POST['password'])) {
-
+//timea ma velkz kar 
         //connect string do DB
         $servername = "localhost";
         $username = "Klopcekova";
